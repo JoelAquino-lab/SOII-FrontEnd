44eede935aa8083ba191da1d72f378e94f9df048
Joel Calidonio <joelaquino8181@gmail.com>
2026-07-25 09:47:54 -0600

    Descripción de los cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  //apiUrl: 'http://localhost:3000',
-  apiUrl: 'https://sobackend.darlinng5.workers.dev',
+  apiUrl: 'http://localhost:3000',
+  //apiUrl: 'https://sobackend.darlinng5.workers.dev',
 };
